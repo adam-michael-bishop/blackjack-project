0923df396f37482370dee39df5cbcb8aa5ba61e8
Adam Bishop <adam.michael.bishop@gmail.com>
2022-12-19 17:55:54 -0600

    Main.js now handles the flow of the game and displaying graphics, game.js handles game logic

---
+++ js/game.js
@@ -0,0 +1,240 @@
+"use strict";
+
+import * as Cards from "./cards.js";
+// import * as Render from "./render.js";
+
+export {player, dealer, startHand, deal};
+
+const handMethods = {
+	getHandTotal: function () {
+		let handTotal = 0;
+		for (const card of this.hand) {
+			handTotal += card.rank.value;
+		}
+		while (handTotal > Cards.blackjack && this.hand.some(e => e.rank.id === "ace" && e.rank.value !== Cards.aceConditionalValue)){
+			//find the index of an object that contains the property id === "ace" and has not already been set to 1
+			let aceIndex = this.hand.findIndex(e => e.rank.id === "ace" && e.rank.value !== Cards.aceConditionalValue);
+			handTotal -= this.hand[aceIndex].rank.value;
+			handTotal += Cards.aceConditionalValue;
+			this.hand[aceIndex].rank.value = Cards.aceConditionalValue;
+		}
+		return handTotal;
+	},
+	isHandSoft: function () {
+		return this.hand.some(e => e.rank.id === "ace" && e.rank.value === Cards.aceDefaultValue);
+	},
+	resetAceValue: function () {
+		for (const card of this.hand) {
+			if (card.rank.id === "ace") {
+				card.rank.value = Cards.aceDefaultValue;
+			}
+		}
+	},
+	getHandAsString: function () {
+		let handString = '';
+		this.hand.forEach(function (card, index, arr) {
+			if (index === arr.length - 1) {
+				handString += `${card.rank.id} of ${card.suit}s`;
+			} else {
+				handString += `${card.rank.id} of ${card.suit}s, `;
+			}
+		});
+		return handString;
+	},
+	returnHandToDeck: function (arr) {
+		while (this.hand.length > 0) {
+			arr.push(this.hand.pop());
+		}
+	}
+}
+
+const player = {
+	hand: [],
+	score: 0,
+	__proto__: handMethods
+};
+
+const dealer = {
+	hand: [],
+	score: 0,
+	turn: false,
+	standAt: 17,
+	__proto__: handMethods
+};
+
+const dealerStandAt = 17;
+let playingHand = false;
+let dealerTurn = false;
+let deck = Cards.deck;
+
+// function initGame(){
+// 	if(playingHand){
+// 		playGame();
+// 	} else {
+// 		Render.displayMainMenu();
+// 	}
+// }
+
+// function displayMainMenu(){
+// 	do {
+// 		let playerMenuInput = prompt("Enter 1 or start to play Blackjack, enter 2 or exit to exit game...");
+
+// 		if(playerMenuInput === "1") {
+// 			playingHand = true;
+// 			playGame();
+// 			return;
+// 		} else if(playerMenuInput === "2" || playerMenuInput === null) {
+// 			let playerConfirmQuit = confirm("Are you sure you want to quit?");
+// 			if(playerConfirmQuit){
+// 				break;
+// 			}
+// 		}
+// 	} while (!playingHand);
+// }
+
+function startHand() {
+	resetHands();
+	Cards.shuffle(deck);
+	dealerTurn = false;
+	deal(player, 2);
+	deal(dealer, 2);
+}
+
+function playGame(){
+	resetScores();
+	while (playingHand) {
+		resetHands();
+		Cards.shuffle(deck);
+		dealerTurn = false;
+		deal(player, 2);
+		deal(dealer, 2);
+
+		checkForBlackjack();
+		if(dealerTurn) {continue;}
+
+		let playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
+
+		while (playerGameMenuInput !== "1" && playerGameMenuInput !== "2" && playerGameMenuInput !== "3" && playerGameMenuInput !== null){
+			playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
+		}
+
+		if (playerGameMenuInput === "1") {
+			while (!dealerTurn){
+				deal(player);
+				if (player.getHandTotal() > Cards.blackjack){
+					determineHandWinner();
+					break;
+				}
+				do {
+					playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
+					if (playerGameMenuInput === "2" || playerGameMenuInput === "3"){
+						dealerTurn = true;
+						break;
+					}
+				} while (playerGameMenuInput !== "1")
+			}
+		}
+		if (playerGameMenuInput === "2") {
+			dealerTurn = true;
+			while (dealer.getHandTotal() < dealerStandAt) {
+				alert(`${displayHands()}\n \nDealer Hits`);
+				deal(dealer);
+			}
+			determineHandWinner();
+			continue;
+		}
+		if (playerGameMenuInput === "3" || playerGameMenuInput === null) {
+			let playerConfirmQuit = confirm("Are you sure you want to return to Main Menu?");
+			if (playerConfirmQuit) {
+				playingHand = false;
+				displayMainMenu();
+				return;
+			}
+		}
+	}
+}
+
+function resetScores(){
+	player.score = 0;
+	dealer.score = 0;
+}
+
+function printScores(){
+	return `Player Score: ${player.score} Dealer Score: ${dealer.score}`;
+}
+
+function displayHands(){
+	// Render.drawHandToContext(player);
+	let playerHandString = '';
+	let dealerHandString = '';
+
+	if (dealerTurn){
+		dealerHandString = dealer.getHandAsString();
+	} else {
+		dealerHandString = `${dealer.hand[0].rank.id} of ${dealer.hand[0].suit}s`
+	}
+	playerHandString = player.getHandAsString();
+	//Following line is kind of messy, consider refactoring to make it readable.
+	return `${printScores()}\n \nDealer's hand: ${dealerHandString}\n${dealerTurn ? `Dealer Total: ${dealer.getHandTotal()} ${dealer.isHandSoft()? '(Soft)' : ''}\n` : ''}\nYour hand: ${playerHandString}\nYour total: ${player.getHandTotal()} ${player.isHandSoft()? '(Soft)' : ''}`
+}
+
+function checkForBlackjack(){
+	if (dealer.hand[0].rank.value >= 10){
+		alert(`${displayHands()}\n \nChecking for dealer Blackjack...`);
+	}
+	if (player.getHandTotal() === Cards.blackjack || dealer.getHandTotal() === Cards.blackjack){
+		if (player.getHandTotal() === Cards.blackjack){
+			alert("Player Blackjack!");
+		}
+		determineHandWinner();
+	}
+}
+
+function determineHandWinner(){
+	dealerTurn = true;
+	if (player.getHandTotal() === dealer.getHandTotal()){
+		alert(`${displayHands()}\n \nPush`);
+		resetHands();
+		Cards.shuffle(deck);
+	} else if ((player.getHandTotal() > dealer.getHandTotal() && player.getHandTotal() <= Cards.blackjack) || dealer.getHandTotal() > Cards.blackjack){
+		player.score++;
+		alert(`${displayHands()}\n \nYou Win!`);
+		resetHands();
+		Cards.shuffle(deck);
+	} else {
+		dealer.score++;
+		alert(`${displayHands()}\n \nYou Lose`);
+		resetHands();
+		Cards.shuffle(deck);
+	}
+}
+
+function deal(target, cardsToDeal = 1){
+	for (let i = 0; i < cardsToDeal; i++) {
+		target.hand.push(deck.pop());
+	}
+}
+
+function resetHands(){
+	if (player.hand.length === 0 && dealer.hand.length === 0) {return;}
+
+	player.resetAceValue();
+	dealer.resetAceValue();
+	player.returnHandToDeck(deck);
+	dealer.returnHandToDeck(deck);
+}
+
+/**
+ * Testing out drawing a card and rendering on the screen
+ */
+
+// deal(player, 5)
+// Render.drawHandToContext(player);
+//
+// console.log(player.getHandAsString());
+// console.log(player.hand);
+// console.log(player);
+
+Cards.buildDeck();
+Cards.shuffle(deck);
+// initGame();
--- js/main.js
+++ js/main.js
@@ -1,231 +1,119 @@
 "use strict";
 
+import * as Game from "./game.js";
 import * as Cards from "./cards.js";
-import * as Render from "./render.js";
 
+const CARDS_IMAGE_PATH = './assets/sprites/cards';
+const MAIN_GAME_WINDOW = document.getElementById('game-window');
+const PLAYER_HAND_WINDOW_ID = 'player-hand';
+const DEALER_HAND_WINDOW_ID = 'dealer-hand';
+const FACE_DOWN_CARD_PATH = './assets/sprites/cards/back_blue_basic.png'
 
-const handMethods = {
-	getHandTotal: function () {
-		let handTotal = 0;
-		for (const card of this.hand) {
-			handTotal += card.rank.value;
-		}
-		while (handTotal > Cards.blackjack && this.hand.some(e => e.rank.id === "ace" && e.rank.value !== Cards.aceConditionalValue)){
-			//find the index of an object that contains the property id === "ace" and has not already been set to 1
-			let aceIndex = this.hand.findIndex(e => e.rank.id === "ace" && e.rank.value !== Cards.aceConditionalValue);
-			handTotal -= this.hand[aceIndex].rank.value;
-			handTotal += Cards.aceConditionalValue;
-			this.hand[aceIndex].rank.value = Cards.aceConditionalValue;
-		}
-		return handTotal;
-	},
-	isHandSoft: function () {
-		return this.hand.some(e => e.rank.id === "ace" && e.rank.value === Cards.aceDefaultValue);
-	},
-	resetAceValue: function () {
-		for (const card of this.hand) {
-			if (card.rank.id === "ace") {
-				card.rank.value = Cards.aceDefaultValue;
-			}
-		}
-	},
-	getHandAsString: function () {
-		let handString = '';
-		this.hand.forEach(function (card, index, arr) {
-			if (index === arr.length - 1) {
-				handString += `${card.rank.id} of ${card.suit}s`;
-			} else {
-				handString += `${card.rank.id} of ${card.suit}s, `;
-			}
-		});
-		return handString;
-	},
-	returnHandToDeck: function (arr) {
-		while (this.hand.length > 0) {
-			arr.push(this.hand.pop());
-		}
-	}
+function getCardURL(rank, suit) {
+    return `${CARDS_IMAGE_PATH}/${rank.toLowerCase()}_${suit.toLowerCase()}s_white.png`;
 }
 
-const player = {
-	hand: [],
-	score: 0,
-	__proto__: handMethods
-};
+function createButton(parent, id, label) {
+    const button = document.createElement("button");
 
-const dealer = {
-	hand: [],
-	score: 0,
-	turn: false,
-	standAt: 17,
-	__proto__: handMethods
-};
+    if (id) {
+        button.setAttribute("id", id);
+    }
+    if (label) {
+        button.innerText = label;
+    }
 
-const dealerStandAt = 17;
-let playingHand = false;
-let dealerTurn = false;
-let deck = Cards.deck;
-
-function initGame(){
-	if(playingHand){
-		playGame();
-	} else {
-		Render.displayMainMenu();
-	}
+    parent.appendChild(button);
 }
 
-// function displayMainMenu(){
-// 	do {
-// 		let playerMenuInput = prompt("Enter 1 or start to play Blackjack, enter 2 or exit to exit game...");
-
-// 		if(playerMenuInput === "1") {
-// 			playingHand = true;
-// 			playGame();
-// 			return;
-// 		} else if(playerMenuInput === "2" || playerMenuInput === null) {
-// 			let playerConfirmQuit = confirm("Are you sure you want to quit?");
-// 			if(playerConfirmQuit){
-// 				break;
-// 			}
-// 		}
-// 	} while (!playingHand);
-// }
-
-function playGame(){
-	resetScores();
-	while (playingHand) {
-		resetHands();
-		Cards.shuffle(deck);
-		dealerTurn = false;
-		deal(player, 2);
-		deal(dealer, 2);
-
-		checkForBlackjack();
-		if(dealerTurn) {continue;}
-
-		let playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
-
-		while (playerGameMenuInput !== "1" && playerGameMenuInput !== "2" && playerGameMenuInput !== "3" && playerGameMenuInput !== null){
-			playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
-		}
-
-		if (playerGameMenuInput === "1") {
-			while (!dealerTurn){
-				deal(player);
-				if (player.getHandTotal() > Cards.blackjack){
-					determineHandWinner();
-					break;
-				}
-				do {
-					playerGameMenuInput = prompt(`${displayHands()}\n \nSelect 1: Hit | 2: Stand | 3: Main Menu`);
-					if (playerGameMenuInput === "2" || playerGameMenuInput === "3"){
-						dealerTurn = true;
-						break;
-					}
-				} while (playerGameMenuInput !== "1")
-			}
-		}
-		if (playerGameMenuInput === "2") {
-			dealerTurn = true;
-			while (dealer.getHandTotal() < dealerStandAt) {
-				alert(`${displayHands()}\n \nDealer Hits`);
-				deal(dealer);
-			}
-			determineHandWinner();
-			continue;
-		}
-		if (playerGameMenuInput === "3" || playerGameMenuInput === null) {
-			let playerConfirmQuit = confirm("Are you sure you want to return to Main Menu?");
-			if (playerConfirmQuit) {
-				playingHand = false;
-				displayMainMenu();
-				return;
-			}
-		}
-	}
+function displayMainMenu() {
+    MAIN_GAME_WINDOW.replaceChildren();
+    let text = document.createElement('div');
+    text.innerText = 'Blackjack!!!';
+    MAIN_GAME_WINDOW.appendChild(text);
+    createButton(MAIN_GAME_WINDOW, "start", "Start Game");
+    createButton(MAIN_GAME_WINDOW, "exit", "Exit Game");
+    document.querySelector("#start").addEventListener('click', displayPlayerTurnMenu);
 }
 
-function resetScores(){
-	player.score = 0;
-	dealer.score = 0;
+function displayPlayerTurnMenu() {
+
+    let playerHandWindow = document.createElement('div');
+    let dealerHandWindow = document.createElement('div');
+
+    MAIN_GAME_WINDOW.replaceChildren();
+    Game.startHand();
+
+    dealerHandWindow.id = DEALER_HAND_WINDOW_ID;
+    MAIN_GAME_WINDOW.appendChild(dealerHandWindow);
+    playerHandWindow.id = PLAYER_HAND_WINDOW_ID;
+    MAIN_GAME_WINDOW.appendChild(playerHandWindow);
+
+    displayHand(Game.player, playerHandWindow);
+    displayDealerFaceUpCard();
+
+    createButton(MAIN_GAME_WINDOW, "hit", "Hit");
+    createButton(MAIN_GAME_WINDOW, "stand", "Stand");
+    createButton(MAIN_GAME_WINDOW, "quit", "Return to Main Menu");
+
+    document.querySelector("#hit").addEventListener("click", hit);
+    document.querySelector("#stand").addEventListener("click", stand);
+    document.querySelector("#quit").addEventListener("click", displayMainMenu);
 }
 
-function printScores(){
-	return `Player Score: ${player.score} Dealer Score: ${dealer.score}`;
+function hit() {
+    Game.deal(Game.player);
+    let playerHandWindow = document.getElementById(PLAYER_HAND_WINDOW_ID);
+    displayHand(Game.player, playerHandWindow);
+    if (Game.player.getHandTotal() > Cards.blackjack) {
+        console.log("you lose");
+        displayHandOverMenu();
+    }
+    console.log(Game.player.hand);
 }
 
-function displayHands(){
-	// Render.drawHandToContext(player);
-	let playerHandString = '';
-	let dealerHandString = '';
-
-	if (dealerTurn){
-		dealerHandString = dealer.getHandAsString();
-	} else {
-		dealerHandString = `${dealer.hand[0].rank.id} of ${dealer.hand[0].suit}s`
-	}
-	playerHandString = player.getHandAsString();
-	//Following line is kind of messy, consider refactoring to make it readable.
-	return `${printScores()}\n \nDealer's hand: ${dealerHandString}\n${dealerTurn ? `Dealer Total: ${dealer.getHandTotal()} ${dealer.isHandSoft()? '(Soft)' : ''}\n` : ''}\nYour hand: ${playerHandString}\nYour total: ${player.getHandTotal()} ${player.isHandSoft()? '(Soft)' : ''}`
+function stand() {
+    //do stuff;
 }
 
-function checkForBlackjack(){
-	if (dealer.hand[0].rank.value >= 10){
-		alert(`${displayHands()}\n \nChecking for dealer Blackjack...`);
-	}
-	if (player.getHandTotal() === Cards.blackjack || dealer.getHandTotal() === Cards.blackjack){
-		if (player.getHandTotal() === Cards.blackjack){
-			alert("Player Blackjack!");
-		}
-		determineHandWinner();
-	}
+function displayHand(target, displayWindow) {
+    displayWindow.replaceChildren();
+    for (const card of target.hand) {
+        let myCard = document.createElement('img');
+        myCard.src = getCardURL(card.rank.id, card.suit);
+        displayWindow.appendChild(myCard);
+    }
 }
 
-function determineHandWinner(){
-	dealerTurn = true;
-	if (player.getHandTotal() === dealer.getHandTotal()){
-		alert(`${displayHands()}\n \nPush`);
-		resetHands();
-		Cards.shuffle(deck);
-	} else if ((player.getHandTotal() > dealer.getHandTotal() && player.getHandTotal() <= Cards.blackjack) || dealer.getHandTotal() > Cards.blackjack){
-		player.score++;
-		alert(`${displayHands()}\n \nYou Win!`);
-		resetHands();
-		Cards.shuffle(deck);
-	} else {
-		dealer.score++;
-		alert(`${displayHands()}\n \nYou Lose`);
-		resetHands();
-		Cards.shuffle(deck);
-	}
+function displayDealerFaceUpCard() {
+    let dealerHandWindow = document.getElementById(DEALER_HAND_WINDOW_ID);
+    let faceUpCard = document.createElement('img');
+    let faceDownCard = document.createElement('img');
+
+    faceUpCard.src = getCardURL(Game.dealer.hand[0].rank.id, Game.dealer.hand[0].suit);
+    dealerHandWindow.appendChild(faceUpCard);
+
+    faceDownCard.src = FACE_DOWN_CARD_PATH;
+    dealerHandWindow.appendChild(faceDownCard);
 }
 
-function deal(target, cardsToDeal = 1){
-	for (let i = 0; i < cardsToDeal; i++) {
-		target.hand.push(deck.pop());
-	}
+function removeAllButtons() {
+    let buttons = Array.from(MAIN_GAME_WINDOW.getElementsByTagName('button'));
+    
+    for (const button of buttons) {
+        button.remove();
+    }
 }
 
-function resetHands(){
-	if (player.hand.length === 0 && dealer.hand.length === 0) {return;}
+function displayHandOverMenu() {
+    removeAllButtons();
 
-	player.resetAceValue();
-	dealer.resetAceValue();
-	player.returnHandToDeck(deck);
-	dealer.returnHandToDeck(deck);
+    createButton(MAIN_GAME_WINDOW, "play-again", "Play Again");
+    createButton(MAIN_GAME_WINDOW, "quit", "Return to Main Menu");
+
+    document.querySelector("#play-again").addEventListener("click", displayPlayerTurnMenu);
+    document.querySelector("#quit").addEventListener("click", displayMainMenu);
 }
 
-/**
- * Testing out drawing a card and rendering on the screen
- */
 
-// deal(player, 5)
-// Render.drawHandToContext(player);
-//
-// console.log(player.getHandAsString());
-// console.log(player.hand);
-// console.log(player);
-
-Cards.buildDeck();
-Cards.shuffle(deck);
-initGame();
+displayMainMenu();
